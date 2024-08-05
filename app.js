cb5800644f9d38e3166ec423ba1bf7640b25522a
tupe-atharva <atharva.tupe007@gmail.com>
2024-08-05 23:29:42 +0530

    added generateCompChoice func and restart function

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+//globally declaring variables to store user's and computer's score
 let compScore = 0;
 let userScore = 0;
 //declare these variables globally outside the loop as if these are written inside the loop then everytime the loop runs , the variables are set to 0 again and again
 
+//accessing elements using DOM
 let compScoreDisplay = document.querySelector("#comp")
 let userScoreDisplay = document.querySelector("#user")
-
 let img = document.querySelectorAll("img")
 // console.log(img)
-let generateCompChoice=()=>{
 
+let generateCompChoice=()=>{
+    let i = Math.random() //this Math.random() function generates random number such that 0>=num && num<1
+    let idx=Math.floor(i*3) //here we have multiplied the generated number bny 3 and taken the floor part of that number , in this way we get 0/1/2
+    // eg. say num=0.3
+    // num * 3 = 0.9
+    // its floor value is 0
+    
+    let options = ["rock" , "paper" , "scissors"];
+    //now lets pass the randomly generated number as the index to the array - options and return the value
+    return options[idx]
+    
 }
 //this function should return a string value which will be passed to compSelected and that string value is compared with user input value to generate result
 
@@ -16,12 +27,13 @@ img.forEach((i)=>{
     let userChoice = i.getAttribute("id")
     // console.log(userChoice); these are used to check outputs of the userChoice as a part of good debugging principle.
     // console.log(i);
+    //lets create a event for everytime the image is clicked
     i.addEventListener("click" , ()=>{
         let userSelected = userChoice;
         let playBtn = document.querySelector("#play")
         // console.log(userSelected);
 
-        let compSelected = "rock";
+        let compSelected = generateCompChoice();//returns a string from ["rock","paper","scissors"]
         
         // this if-else ladder compares the STRING value of variables compSelected and userSelected and prints the win msg on the button
         if(compSelected == "rock" && userSelected == "rock"){
@@ -72,18 +84,11 @@ img.forEach((i)=>{
         
         // console.log(compScore); dry check to see if compScore is being displayed correctly
 
-        
-
         compScoreDisplay.innerText =  compScore ;
         userScoreDisplay.innerText = userScore;
-
-
         // console.log(`${userChoice} was clicked`);
-    }) //here i, is a temp var whose job is to access the images in the img nodelist, i returns an html code for the element, whereas the getAttribute function returns the attribute value in string format
-
-    
-
-    
+    }) 
+//here i, is a temp var whose job is to access the images in the img nodelist, i returns an html code for the element, whereas the getAttribute function returns the attribute value in string format    
 })
 
 // logic for resetting the game using new game button
